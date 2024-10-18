c066435fece51af29616a321201f59cc98e4f7a4
dmamanipar <dmamanipar@unsa.edu.pe>
2024-10-18 11:13:42 -0500

    Reporte y Formulario

--- SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/SysAlmacenFxApplication.java
+++ SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/SysAlmacenFxApplication.java
@@ -5,15 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import pe.edu.upeu.sysalmacenfx.pruebas.MainY;
 
 @SpringBootApplication
 public class SysAlmacenFxApplication extends Application {
